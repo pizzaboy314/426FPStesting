4b537b83bc554c8b259e6a266c120010dc016baa
Bryan Spahr <bspahr314@gmail.com>
2014-02-16 23:53:23 -0600

    wooden ball launching, collisions, explosions, fire effects

--- Assets/Scripts/FP_Shooting.cs
+++ Assets/Scripts/FP_Shooting.cs
@@ -3,15 +3,20 @@ using System.Collections;
 
 public class FP_Shooting : MonoBehaviour {
 
+	public GameObject thermal_det;
+	public float detImpulse;
+
 	// Use this for initialization
 	void Start () {
-	
+		detImpulse = 50.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButton("Fire1")){
-
+		if (Input.GetButtonDown("Fire1")){
+			Camera cam = Camera.main;
+			GameObject det = (GameObject)Instantiate(thermal_det, cam.transform.position + cam.transform.forward, cam.transform.rotation);
+			det.rigidbody.AddForce(cam.transform.forward * detImpulse, ForceMode.Impulse);
 		}
 	}
 }
---
+++ Assets/Scripts/thermal_det.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class thermal_det : MonoBehaviour {
+
+	public float lifeSpan;
+	public GameObject fireEffect;
+
+	// Use this for initialization
+	void Start () {
+		lifeSpan = 5.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lifeSpan -= Time.deltaTime;
+
+		if(lifeSpan <= 0){
+			Explode();
+		}
+	}
+
+	void OnCollisonEnter(Collision collision){
+		Debug.Log("collision");
+		if(collision.gameObject.tag == "Enemy"){
+			collision.gameObject.tag = "Dead";
+			Explode();
+		}
+	}
+
+	void Explode(){
+		Instantiate (fireEffect, transform.position, Quaternion.identity);
+		Destroy(gameObject);
+	}
+}
